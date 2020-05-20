37d24431bdc97aa0efc24484e5e6c821da4d6ed7
MKS-01 <manish10mks@gmail.com>
2020-05-20 20:46:10 +0530

    Initial Commit

---
+++ iOS-FullScreenModal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-FullScreenModal
+//
+//  Created by MKS on 20/05/20.
+//  Copyright © 2020 MKS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
